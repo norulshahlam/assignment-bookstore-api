efb0761f87ddd770377d7ef0c21fc5249dc4f58f
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2025-05-31 08:24:06 +0800

    code inspection

--- src/main/java/com/shah/bookstoreapi/exception/MyException.java
+++ src/main/java/com/shah/bookstoreapi/exception/MyException.java
@@ -2,7 +2,9 @@ package com.shah.bookstoreapi.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
 public class MyException extends RuntimeException{
